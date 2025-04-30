99fb0a31994b3f56c9d7f0553fd99a5ea0fc9927
Jack Rathwell <jrathwell@lcs.on.ca>
2025-04-30 12:01:58 -0400

    Initial Commit

---
+++ Foodie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Foodie
+//
+//  Created by Jack William Rathwell on 2025-04-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Jack William Rathwell on 2025-04-30.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
